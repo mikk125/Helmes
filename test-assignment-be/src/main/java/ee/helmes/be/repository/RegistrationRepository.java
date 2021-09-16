c08db126e5dc0091f2de8cefe28da0a0f5a0927a
mikk sillamaa <mikk.sillamaa2gmail.com>
2021-09-16 16:53:46 +0300

    Removed backend

--- test-assignment-be/src/main/java/ee/helmes/be/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.helmes.be.repository;
-
-import ee.helmes.be.model.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-}
