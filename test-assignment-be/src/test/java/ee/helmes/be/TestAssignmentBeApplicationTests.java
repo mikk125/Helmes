14cac65047fec8f06a8a5115095b66b190838138
mikk sillamaa <mikk.sillamaa2gmail.com>
2021-09-16 17:27:41 +0300

    updated backend

---
+++ test-assignment-be/src/test/java/ee/helmes/be/TestAssignmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.helmes.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
